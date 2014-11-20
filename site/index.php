39710a63a0478a78f8d9c39799e0a09ba8458383
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2014-11-19 23:25:15 -0200

    insercao de canvas em modal window

--- site/index.php
+++ site/index.php
@@ -199,22 +199,10 @@
                         <div class="modal-body">
                             <h2>Project Title</h2>
                             <hr class="star-primary">
-                            <img src="img/portfolio/cabin.png" class="img-responsive img-centered" alt="">
-                            <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
-                            <ul class="list-inline item-details">
-                                <li>Client:
-                                    <strong><a href="http://startbootstrap.com">Start Bootstrap</a>
-                                    </strong>
-                                </li>
-                                <li>Date:
-                                    <strong><a href="http://startbootstrap.com">April 2014</a>
-                                    </strong>
-                                </li>
-                                <li>Service:
-                                    <strong><a href="http://startbootstrap.com">Web Development</a>
-                                    </strong>
-                                </li>
-                            </ul>
+                             <canvas id="myCanvas" width="90%" height="90%" style="border:1px solid #000000;">
+                            </canvas> 
+                            
+                            
                             <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
                         </div>
                     </div>
@@ -223,7 +211,12 @@
         </div>
     </div>
    
-     
+      <script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
+</script> 
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
